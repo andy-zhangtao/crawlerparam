286efe8f8711855e29e4ca35ce6bded483405aa7
andy <andy.zhangtao@hotmail.com>
2017-11-07 17:08:32 +0800

    增加ID
    Commit by Andy.Zhang
    Email: ztao8607@gmail.com

--- v1/struct.go
+++ v1/struct.go
@@ -1,18 +1,19 @@
 package v1
 
-type Doc struct{
-	Title string `json:"title"`
-	Desc string `json:"desc"`
-	Url string `json:"url"`
-	Img []Doc_img `json:"img"`
-	Source int `json:"source"`
-	Keys string `json:"keys"`
-	Upload string `json:"upload"`
-	Chanid int `json:"chanid"`
-	Isparsed bool `json:"isparsed"`
+type Doc struct {
+	ID       string    `json:id`
+	Title    string    `json:"title"`
+	Desc     string    `json:"desc"`
+	Url      string    `json:"url"`
+	Img      []Doc_img `json:"img"`
+	Source   int       `json:"source"`
+	Keys     string    `json:"keys"`
+	Upload   string    `json:"upload"`
+	Chanid   int       `json:"chanid"`
+	Isparsed bool      `json:"isparsed"`
 }
-type Doc_img struct{
-	Url string `json:"url"`
-	Width int `json:"width"`
-	Height int `json:"height"`
+type Doc_img struct {
+	Url    string `json:"url"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
 }
